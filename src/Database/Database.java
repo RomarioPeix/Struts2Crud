d88fd8002b0b780d02e8188d15cdb48e82aec850
romariopeix <jrdapgro@gmail.com>
2019-06-01 00:04:04 -0300

    database class

---
+++ src/Database/Database.java
@@ -0,0 +1,76 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+
+	public static String status = "Não conectou...";
+	public Database() {
+		System.out.println("called");
+	}
+	
+	public static java.sql.Connection getConexaoMySQL() {
+	 
+	    Connection connection = null;
+		try {
+			String driverName = "com.mysql.jdbc.Driver";                        
+			Class.forName(driverName);
+		 
+		    String serverName = "localhost";
+		    String mydatabase = "mysql"; //bd name
+		    String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
+		    String username = "root";
+		    String password = "";
+		    connection = DriverManager.getConnection(url, username, password);
+		 
+		    if (connection != null) {
+		        status = ("STATUS--->Conectado com sucesso!");
+		    } else {
+		        status = ("STATUS--->Não foi possivel realizar conexão");
+		    }
+		 
+		    return connection;
+		
+		} catch (ClassNotFoundException e) {
+	        System.out.println("O driver expecificado nao foi encontrado.");
+	        return null;
+	    } catch (SQLException e) {
+	    	System.out.println("Nao foi possivel conectar ao Banco de Dados."); 
+	        return null;
+	    }
+	
+	}
+	
+	public static String statusConection() {
+	
+		return status;
+	
+	}
+	
+	public static boolean FecharConexao() {
+	
+		try {
+	
+			Database.getConexaoMySQL().close();
+	
+			return true;
+	
+		} catch (SQLException e) {
+	
+			return false;
+	
+		}
+	
+	}
+	
+	public static java.sql.Connection ReiniciarConexao() {
+	
+		FecharConexao();
+	
+		return Database.getConexaoMySQL();
+	
+	}
+
+}
